c3e7466fc4f02a8697cf32cfc47c53cf2036df6e
Gael Emilio Mercado Chanes <emiliotorresemiliot@comunidad.unam.mx>
2026-01-15 19:16:59 +0000

    Subida de archivos de la práctica

--- js/request.js
+++ js/request.js
@@ -1,22 +1,72 @@
-
-var data ={}
+var data = {};
 var xhr = new XMLHttpRequest();
 var requestUrl = "https://fakestoreapi.com/products";
-tbody= document.getElementById("tableBodyData");
-btnRemove=document.getElementById("btnRemove");
-btnRemove.addEventListener("click",function(e){
-    tbody.innerHTML = "";
-});
 
-//consumir api
-function getData(url){
-    xhr.open("GET", requestUrl, true); 
-    xhr.onload = function(url){
-        console.log(xhr.responseText);
-        // Handle data
-        array = JSON.parse(xhr.responseText);
-        data.json=array;
+var tbody = document.getElementById("tableBodyData");
+var btnRemove = document.getElementById("btnRemove");
+var btnLoad = document.getElementById("btnLoad");
+var btnSearch = document.getElementById("btnSearch");
+var inputSearch = document.getElementById("inputSearch");
+
+function genTr(json) {
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
+    let td5 = document.createElement("td");
+
+    td1.innerText = json.id;
+    td2.innerText = json.title;
+    td3.innerText = json.price;
+    td4.innerText = json.description;
+    td5.innerText = json.category;
+
+    tr.append(td1, td2, td3, td4, td5);
+    return tr;
+}
+
+// consumir api
+function getData() {
+    xhr.open("GET", requestUrl, true);
+    xhr.onload = function() {
+        let array = JSON.parse(xhr.responseText);
+        data.json = array;
+
+        tbody.innerHTML = "";
+
+        array.forEach(element => {
+            console.log(element); 
+            tbody.append(genTr(element));
+        });
     };
     xhr.send();
 }
-getData();
+
+// Paso 3
+btnLoad.addEventListener("click", function() {
+    getData();
+    btnLoad.classList.replace("btn-outline-primary", "btn-primary");
+});
+
+btnRemove.addEventListener("click", function(e) {
+    tbody.innerHTML = "";
+});
+
+// Paso 4
+btnSearch.addEventListener("click", function() {
+    if (data.json) {
+        let filtered = data.json.filter(function(e) {
+            return e.title.toLowerCase().includes(inputSearch.value.toLowerCase());
+        });
+
+        console.log(filtered);
+
+        tbody.innerHTML = "";
+        filtered.forEach(element => {
+            tbody.append(genTr(element));
+        });
+    } else {
+        alert("Primero carga los datos con 'Load Data'");
+    }
+});
